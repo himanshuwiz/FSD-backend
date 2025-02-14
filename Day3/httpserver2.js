265b7c86fb2e7907b0ddb7fc846a2dce76b1a6ba
himanshuwiz <himdhi2003@gmail.com>
2025-02-14 12:37:27 +0530

    files-added

--- Day3/httpserver2.js
+++ Day3/httpserver2.js
@@ -1,16 +1,17 @@
 const http = require("http");
 const server = http.createServer(async (req, res) => {
-  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Content-Type", "text.html");
   res.statusCode = 200;
   const data = await fetch("https://dummyjson.com/recipes");
-  const datajson = data.json;
+  const datajson = await data.json();
   const dataarray = datajson.recipes;
-  const names = dataarray.map((recipe) => {
-    return recipe.name;
+  dataarray.map((recipe) => {
+    const data = "<h3 style='color:blue'>" + recipe.name + "<h3>";
+    return res.write(data);
   });
-  res.end(names);
+  res.end();
 });
-server.listen(3001, (err) => {
+server.listen(3030, (err) => {
   try {
     if (err) throw err;
   } catch (err) {
